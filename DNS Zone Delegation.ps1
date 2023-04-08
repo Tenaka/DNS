10241b673675f5a8fa62241460f8d31a3e599f5b
Tenaka <86342641+Tenaka@users.noreply.github.com>
2023-04-08 16:25:43 +0100

    Update DNS Zone Delegation.ps1

--- DNS Zone Delegation.ps1
+++ DNS Zone Delegation.ps1
@@ -1,15 +1,11 @@
-﻿<#
+<#
 Delegates administrative functions to the DNS Servers and Zones without using DNSAdmins
 Each forward and reverse zone is delegated by an individual AD group that also provides READ access to the DNS servers
-
-Top level access to the DNS servers is via AT_DNSServer_MicrosoftDNS_Manage
-To create Zones is via AT_DNSZone_MicrosoftDNS_Manage
-To manage each zone is via AT_DNSZone_$($zoneName)_Manage
-
+Top level access to fully manage the DNS servers is via AT_DNS_MicrosoftDNS_Manage
 
 Requires: 
 AD powershell modules
-Update the DN to reflect where AD Groups will be created
+Update the OU DN to reflect where AD Groups will be created
 #>
 
 Import-Module activedirectory    
@@ -28,6 +24,9 @@ $rootDNSName = $gtRootDns.Name
 #Get DN for Forward and Reverse level - Required for creating new zones
 $gtRootDNSZone = Get-Item "CN=MicrosoftDNS,DC=DomainDnsZones,$($rootDSE)"
 $rootDNSZoneName = $gtRootDNSZone.name
+
+#RootHints DN
+$gtRootDNSHint = "DC=RootDNSServers,CN=MicrosoftDNS,DC=DomainDnsZones,$($rootDSE)"
 
 #UPDATE - update the DN to reflect where AD Groups will be created
 $ouDelegation = "OU=Delegation,OU=Resources,$($rootDSE)"
@@ -58,7 +57,7 @@ foreach ($zone in $gtDnsZones)
     $dnsACL.AddAccessRule($ACE)
     Set-Acl -Path $zoneDN -AclObject $dnsACL
 
-    #RootServer DN and Set READ for the AD Gropus - Allows conection to the DNS Server
+    #RootServer DN and Set READ for the AD Groups - Allows conection to the DNS Server
     $dnsRtACL = Get-Acl -Path $gtRootDnsZones
 
     $ActiveDirectoryRights = [System.DirectoryServices.ActiveDirectoryRights]::GenericRead
@@ -71,6 +70,7 @@ foreach ($zone in $gtDnsZones)
     Set-Acl -Path $gtRootDnsZones -AclObject $dnsRtACL
 }
 
+    #SERVER Manage
     #Create AD Group for delegting FULL control of the DNS Servers
     $adGpName = "AT_DNSServer_$($rootDNSName)_Manage"
     New-ADGroup -Name $adGpName -GroupScope Global -Path $ouDelegation
@@ -81,7 +81,6 @@ foreach ($zone in $gtDnsZones)
 
     #RootServer DN and Set FULL Control
     $dnsRtACL = Get-Acl -Path $gtRootDns
-
     $ActiveDirectoryRights = [System.DirectoryServices.ActiveDirectoryRights]::GenericAll
     $AccessControlType = [System.Security.AccessControl.AccessControlType]::Allow
     $ObjectType = [guid] "00000000-0000-0000-0000-000000000000"
@@ -91,8 +90,20 @@ foreach ($zone in $gtDnsZones)
     $dnsRtACL.AddAccessRule($ACE)
     Set-Acl -Path $gtRootDns -AclObject $dnsRtACL
 
+    #RootHint DN and Set FULL Control
+    $dnsRtACL = Get-Acl -Path $gtRootDNSHint
+    $ActiveDirectoryRights = [System.DirectoryServices.ActiveDirectoryRights]::GenericAll
+    $AccessControlType = [System.Security.AccessControl.AccessControlType]::Allow
+    $ObjectType = [guid] "00000000-0000-0000-0000-000000000000"
+    $InheritanceType = [System.DirectoryServices.ActiveDirectorySecurityInheritance]::SelfAndChildren
+    $InheritedObjectType = [guid] "00000000-0000-0000-0000-000000000000"
+    $ACE = New-Object System.DirectoryServices.ActiveDirectoryAccessRule $GroupSID,$ActiveDirectoryRights, $AccessControlType, $ObjectType, $InheritanceType, $InheritedObjectType
+    $dnsRtACL.AddAccessRule($ACE)
+    Set-Acl -Path $gtRootDNSHint -AclObject $dnsRtACL
 
 
+
+    #ZONE Manage
     #Create AD Group for delegting permissions to create Zones
     $adGpName = "AT_DNSZone_$($rootDNSZoneName)_Manage"
     New-ADGroup -Name $adGpName -GroupScope Global -Path $ouDelegation
@@ -103,13 +114,24 @@ foreach ($zone in $gtDnsZones)
 
     #RootServer DN and Set FULL Control
     $dnsRtACL = Get-Acl -Path $gtRootDNSZone
-
     $ActiveDirectoryRights = [System.DirectoryServices.ActiveDirectoryRights]::GenericAll
+    $AccessControlType = [System.Security.AccessControl.AccessControlType]::Allow
+    $ObjectType = [guid] "00000000-0000-0000-0000-000000000000"
+    $InheritanceType = [System.DirectoryServices.ActiveDirectorySecurityInheritance]::None
+    $InheritedObjectType = [guid] "00000000-0000-0000-0000-000000000000"
+    $ACE = New-Object System.DirectoryServices.ActiveDirectoryAccessRule $GroupSID,$ActiveDirectoryRights, $AccessControlType, $ObjectType, $InheritanceType, $InheritedObjectType
+    $dnsRtACL.AddAccessRule($ACE)
+    Set-Acl -Path $gtRootDNSZone -AclObject $dnsRtACL
+
+    #RootServer DN and Set READ for the AD Groups - Allows conection to the DNS Server
+    $dnsRtACL = Get-Acl -Path $gtRootDnsZones
+    $ActiveDirectoryRights = [System.DirectoryServices.ActiveDirectoryRights]::GenericRead
     $AccessControlType = [System.Security.AccessControl.AccessControlType]::Allow
     $ObjectType = [guid] "00000000-0000-0000-0000-000000000000"
     $InheritanceType = [System.DirectoryServices.ActiveDirectorySecurityInheritance]::SelfAndChildren
     $InheritedObjectType = [guid] "00000000-0000-0000-0000-000000000000"
     $ACE = New-Object System.DirectoryServices.ActiveDirectoryAccessRule $GroupSID,$ActiveDirectoryRights, $AccessControlType, $ObjectType, $InheritanceType, $InheritedObjectType
     $dnsRtACL.AddAccessRule($ACE)
-    Set-Acl -Path $gtRootDNSZone -AclObject $dnsRtACL
+    Set-Acl -Path $gtRootDnsZones -AclObject $dnsRtACL
+
 
